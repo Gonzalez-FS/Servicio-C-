f2790b5ce62d6d3c40d5e48f82a8c6fca3897e63
Gonzalez-FS <108537743+Gonzalez-FS@users.noreply.github.com>
2022-09-27 09:39:18 -0300

    Add files via upload

---
+++ WindowsService3/ProjectInstaller.Designer.cs
@@ -0,0 +1,58 @@
+﻿
+namespace WindowsService3
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller3 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller3 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller3
+            // 
+            this.serviceProcessInstaller3.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.serviceProcessInstaller3.Password = null;
+            this.serviceProcessInstaller3.Username = null;
+            // 
+            // serviceInstaller3
+            // 
+            this.serviceInstaller3.ServiceName = "Service3";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller3,
+            this.serviceInstaller3});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller3;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller3;
+    }
+}
---
+++ WindowsService3/Service3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Timers;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsService3
+{
+    
+    public partial class Service3 : ServiceBase
+    {
+        public System.Timers.Timer TimerServicio = new System.Timers.Timer();
+        public Service3()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            TimerServicio = new System.Timers.Timer();
+            TimerServicio.Elapsed += (_, __) => EjecutaUnaAccion();
+            TimerServicio.Interval = 60000;
+            TimerServicio.Start();
+        }
+        public void EjecutaUnaAccion()
+        {
+            int i;
+            for (i = 1; i <= 1000; i++)
+{
+                File.AppendAllText(@"C:\WindowsService3\bin\Debug\InformeC#.TXT", "Línea C# N° " + i + System.Environment.NewLine);
+} 
+       }
+        protected override void OnStop()
+        {
+            TimerServicio.Close();
+        }
+        protected override void OnPause()
+        {
+            TimerServicio.Stop();
+        }
+        protected override void OnContinue()
+        {
+            TimerServicio.Start();
+        }
+    }
+}
